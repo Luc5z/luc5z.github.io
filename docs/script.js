802a3bc62696f7934aacb10912021fae9c3db8cf
Dival Lucas <lusca404@gmail.com>
2025-01-23 16:10:32 -0300

    Updating project, new visual and functions

--- docs/script.js
+++ docs/script.js
@@ -61,61 +61,182 @@ const projetos = {
         repositorio: 'https://github.com/Luc5z/calculadora',
     },
 
-  };
-  const certificados = {
+};
+
+const certificados = {
     sql: {
+      id: 'sql-degree', 
       foto: 'images/certificates/curso_sql.jpg',
+      titulo: 'SQL',
+      class: 'degree',
     },
     linux: {
+      id: 'linux-degree',
       foto: 'images/certificates/curso_linux.jpg',
+      titulo: 'Linux',
+      class: 'degree',
     },
     docker: {
+      id: 'docker-degree',
+      titulo: 'Docker',
       foto: 'images/certificates/curso_docker.jpg',
+      class: 'degree',
     },
     agil: {
+      id: 'agil-degree',
+      titulo: 'Agil',
       foto: 'images/certificates/curso_agil.jpg',
+      class: 'degree',
     },
     bigData: {
+      id: 'bigdata-degree',
+      titulo: 'Big Data',
       foto: 'images/certificates/certificado-big-data.jpg',
+      class: 'degree',
     },
     git: {
+      id: 'git-degree',
+      titulo: 'Git',
       foto: 'images/certificates/curso_git.jpg',
+      class: 'degree',
     },
     ingles: {
+      id: 'ingles-degree',
+      titulo: 'Inglês',
       foto: 'images/certificates/ingles.jpg',
+      class: 'degree',
     },
     cloudQuest: {
+      id: 'cloudquest-badge',
+      titulo: 'AWS Cloud Quest - Cloud Practitioner',
       foto: 'images/certificates/aws-cloud-quest-cloud-practitioner.png',
+      class: 'badge',
     },
     accreditationTechnical: {
+      id: 'accreditationtechnical-badge',
+      titulo: 'AWS Partner Accreditation - Technical',
       foto: 'images/certificates/aws-partner-accreditation-technical.png',
+      class: 'badge',
     },
     cloudEconomics: {
+      id: 'cloudeconomics-badge',
+      titulo: 'AWS Partner Cloud Economics Accreditation',
       foto: 'images/certificates/aws-partner-cloud-economics-accreditation.png',
+      class: 'badge',
     },
     salesAccreditation: {
+      id: 'salesaccreditation-badge',
+      titulo: 'AWS Partner Sales Accreditation - Business',
       foto: 'images/certificates/aws-partner-sales-accreditation-business.png',
+      class: 'badge',
     },
 };
 
-  function mostrarModalSobreMim() {
-    document.getElementById('myModalSobreMim').style.display = 'block';
-  }
+const tecnologias = {
+  python: {
+    id: 'python',
+    foto: 'images/technologies/python.png',
+    titulo: 'Python',
+    height: '150px',
+  },
+  django: {
+    id: 'django',
+    foto: 'images/technologies/django.png',
+    titulo: 'Django',
+    height: '150px',
+  },
+  docker: {
+    id: 'docker',
+    foto: 'images/technologies/docker.png',
+    titulo: 'Docker',
+    height: '150px',
+  },
+  aws: {
+    id: 'aws',
+    foto: 'images/technologies/aws.png',
+    titulo: 'AWS',
+    height: '100px',
+  },
+  postgresql: {
+    id: 'postgresql',
+    foto: 'images/technologies/postgre.png',
+    titulo: 'PostgreSQL',
+    height: '100px',
+  },
+  git: {
+    id: 'git',
+    foto: 'images/technologies/git.png',
+    titulo: 'Git',
+    height: '100px',
+  },
+  linux: {
+    id: 'linux',
+    foto: 'images/technologies/linux.png',
+    titulo: 'Linux',
+    height: '100px',
+  },  
+  rubyonrails: {
+    id: 'rubyonrails',
+    foto: 'images/technologies/rubyonrails.png',
+    titulo: 'Ruby on Rails',
+    height: '80px',
+  },
+  ruby: {
+    id: 'ruby',
+    foto: 'images/technologies/ruby.png',
+    titulo: 'Ruby',
+    height: '100px',
+  },
+  bootstrap: {
+    id: 'bootstrap',
+    foto: 'images/technologies/bootstrap.png',
+    titulo: 'Bootstrap',
+    height: '100px',
+  },
+  figma: {
+    id: 'figma',
+    foto: 'images/technologies/figma.png',
+    titulo: 'Figma',
+    height: '100px',
+  },
+  javascript: {
+    id: 'javascript',
+    foto: 'images/technologies/javascript.png',
+    titulo: 'JavaScript',
+    height: '150px',
+  },
+  css: {
+    id: 'css',
+    foto: 'images/technologies/css.png',
+    titulo: 'CSS',
+    height: '100px',
+  },
+  html: {
+    id: 'html',
+    foto: 'images/technologies/html.png',
+    titulo: 'HTML',
+    height: '100px',
+  },
+};
+
+function mostrarModalSobreMim() {
+  document.getElementById('myModalSobreMim').style.display = 'block';
+}
   
-  function fecharModalSobreMim() {
-    document.getElementById('myModalSobreMim').style.display = 'none';
-  }
+function fecharModalSobreMim() {
+  document.getElementById('myModalSobreMim').style.display = 'none';
+}
   
 
-  function trocarAba(div) {
-    document.querySelector('.feed').style.display = 'none';
-    document.querySelector('.certificate').style.display = 'none';
-    document.querySelector('.technology').style.display = 'none';
+function trocarAba(div) {
+  document.querySelector('.projetos').style.display = 'none';
+  document.querySelector('.certificados').style.display = 'none';
+  document.querySelector('.tecnologias').style.display = 'none';
     
-    document.querySelector('.' + div).style.display = 'flex';
-  }
+  document.querySelector('.' + div).style.display = 'flex';
+}
 
-  function carregarProjetos() {
+function carregarprojetos() {
     const container = document.getElementById('feed');
     let htmlContent = '';
 
@@ -135,71 +256,124 @@ const projetos = {
     }
 
     container.innerHTML = htmlContent;
-}
+  }
 
-  function abrirProjeto(idProjeto) {
-    const projeto = projetos[idProjeto];
-    document.getElementById('modalTitle').innerText = projeto.titulo;
-    document.getElementById('modalDescription').innerText = projeto.descricao;
-    const linkRepositorio = document.getElementById('modalRepositorio');
-    linkRepositorio.href = projeto.repositorio; 
+  function carregarcertificados() {
+    const container = document.querySelector('.certificados');
+    let htmlContent = '';
 
-    if (!projeto.repositorio){
-    linkRepositorio.style.display = 'none';
+    for (const chave in certificados) {
+        if (certificados.hasOwnProperty(chave)) {
+            const certificado = certificados[chave];
+
+            htmlContent += `
+                <div id="${certificado.id}" class="${certificado.class}" onclick="abrirCertificado('${chave}')">
+                  <img src="${certificado.foto}" alt="${certificado.titulo}" style="width: 100%; height: 100%; object-fit: fill; cursor: pointer;">
+                </div>
+
+            `;
+        }
     }
 
-    if (!projeto.verOnline){
+    container.innerHTML = htmlContent;
+  }
+
+  function carregartecnologias() {
+    const container = document.querySelector('.tecnologias');
+    let htmlContent = '';
+
+    for (const chave in tecnologias) {
+        if (tecnologias.hasOwnProperty(chave)) {
+          const tecnologia = tecnologias[chave];
+
+          htmlContent += `
+          <div class="tech">
+            <div class="info">
+              <img src=" ${tecnologia.foto} " alt=" ${tecnologia.titulo} " style=" height: ${tecnologia.height} ;">
+              <h1> ${tecnologia.titulo} </h1>
+            </div>
+          
+          </div>
+          `;
+      }
+    }
+
+    container.innerHTML = htmlContent;
+  }
+
+
+function abrirProjeto(idProjeto) {
+  const projeto = projetos[idProjeto];
+  document.getElementById('modalTitle').innerText = projeto.titulo;
+  document.getElementById('modalDescription').innerText = projeto.descricao;
+  const linkRepositorio = document.getElementById('modalRepositorio');
+  linkRepositorio.href = projeto.repositorio; 
+
+  if (!projeto.repositorio){
+    linkRepositorio.style.display = 'none';
+  }
+
+  if (!projeto.verOnline){
     const linkVerOnlineModal = document.getElementById('modalVerOnline');
     linkVerOnlineModal.style.display = 'none';
     linkRepositorio.style.display = projeto.repositorio ? 'block' : 'none';
-    }
+  }
 
-    if (projeto.verOnline) {
-      const video = document.getElementById('video');
-      video.style.display = 'none';
-      const fotoProjeto = document.getElementById('modalImage');
-      fotoProjeto.style.display = 'block';
-      fotoProjeto.src = projeto.foto;
+  if (projeto.verOnline) {
+    const video = document.getElementById('video');
+    video.style.display = 'none';
+    const fotoProjeto = document.getElementById('modalImage');
+    fotoProjeto.style.display = 'block';
+    fotoProjeto.src = projeto.foto;
 
-      const linkVerOnlineModal = document.getElementById('modalVerOnline');
-      linkVerOnlineModal.style.display = 'block';
-      linkVerOnlineModal.href = projeto.verOnline;
-      linkRepositorio.style.display = projeto.repositorio ? 'block' : 'none';
-    }
+    const linkVerOnlineModal = document.getElementById('modalVerOnline');
+    linkVerOnlineModal.style.display = 'block';
+    linkVerOnlineModal.href = projeto.verOnline;
+    linkRepositorio.style.display = projeto.repositorio ? 'block' : 'none';
+  }
 
-    else {
-      const img = document.getElementById('modalImage');
-      img.style.display = 'none';
+  else {
+    const img = document.getElementById('modalImage');
+    img.style.display = 'none';
 
-      const video = document.getElementById('video');
-      video.style.display = 'block';
-      video.src = projeto.video;
-      linkRepositorio.style.display = projeto.repositorio ? 'block' : 'none';
-    }
+    const video = document.getElementById('video');
+    video.style.display = 'block';
+    video.src = projeto.video;
+    linkRepositorio.style.display = projeto.repositorio ? 'block' : 'none';
+  }
 
 
     document.getElementById('myModal').style.display = 'block';
-  }
+}
 
-  function fecharModal() {
-    document.getElementById('myModal').style.display = 'none';
-    const video = document.getElementById('video');
-    video.style.display = 'none';
-    const img = document.getElementById('modalImage');
-    img.style.display = 'none';
-  }
+function fecharModal() {
+  document.getElementById('myModal').style.display = 'none';
+  const video = document.getElementById('video');
+  video.style.display = 'none';
+  const img = document.getElementById('modalImage');
+  img.style.display = 'none';
+}
 
-  function abrirCertificado(idCertificado){
-    const certificado = certificados[idCertificado];
-    const fotoCertificado = document.getElementById('fotoCertificado');
-    fotoCertificado.src = certificado.foto
+function abrirCertificado(idCertificado){
+  const certificado = certificados[idCertificado];
+  const fotoCertificado = document.getElementById('fotoCertificado');
+  fotoCertificado.src = certificado.foto
+
+  document.getElementById('divCertificado').style.display = 'block';
+}
+
+function fecharCertificado(){
+  document.getElementById('divCertificado').style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  carregarprojetos();
+  carregarcertificados();
+  carregartecnologias();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  trocarAba('projetos');
+});
 
 
-    document.getElementById('divCertificado').style.display = 'block';
-  }
-
-  function fecharCertificado(){
-    document.getElementById('divCertificado').style.display = 'none';
-  }
-
-window.onload = carregarProjetos;
